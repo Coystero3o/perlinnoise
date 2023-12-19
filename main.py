b7d6fe884b4e2329a0d26875c60a23c411619552
Coyster <maxzgish@gmail.com>
2023-12-19 21:32:04 +0300

    version 1.1 rewrote cycles as function and changed amplitude, now it looks more realistic

--- main.py
+++ main.py
@@ -3,42 +3,61 @@ from perlin_noise import PerlinNoise
 import matplotlib.pyplot as plt
 import random
 from PIL import Image, ImageDraw
+import time
 
-octaves = 4 # однородность шума (чем больше - тем менее однородный, своего рода зум)
-amp = 10 # количество возможных координат у высоты
-period = 700 # переодичность пиков (чем выше - тем шум более гладкий)
-terrain_width = 1024 # размер поля
-seed = random.randint(1,10000) # сид шума
+def timer(func):
+    def wrapper(landscale, terrain_width, height=None):
+        tb = time.time()
+        if height != None:
+            func(landscale, terrain_width, height)
+        else:
+            func(landscale, terrain_width)
+        te = time.time() - tb
+        print(f"It took {te} seconds to generate")
+
+    return wrapper
+
+@timer
+def fillMatrix(landscale, terrain_width):
+    for position in range(terrain_width ** 2):
+        # вычисление высоты y в координатах (x, z)
+        x = floor(position / terrain_width)
+        z = floor(position % terrain_width)
+        y = floor(noise([x / period, z / period]) * amp)
+        landscale[int(x)][int(z)] = int(y)
+    return landscale
+
+@timer
+def fillPicture(landscale, terrain_width, height):
+    for position in range(terrain_width ** 2):
+        x = floor(position / terrain_width)
+        z = floor(position % terrain_width)
+        if landscale[int(x)][int(z)] < height:
+            draw.point((int(x), int(z)), (0, 149, 182))  # 85, 72, 189
+        elif landscale[int(x)][int(z)] == height:
+            draw.point((int(x), int(z)), (252, 186, 3))
+        elif landscale[int(x)][int(z)] > height:
+            draw.point((int(x), int(z)), (19, 112, 21))
+
+
+octaves = 4  # однородность шума (чем больше - тем менее однородный, своего рода зум)
+amp = 50  # количество возможных координат у высоты
+period = 700  # переодичность пиков (чем выше - тем шум более гладкий)
+terrain_width = 1024  # размер поля
+seed = random.randint(1, 10000)  # сид шума
+image_file = "base.png" # Базовое пустое изображение
+img = Image.open(image_file).convert('RGB')
+draw = ImageDraw.Draw(img)
+pixdata = img.load()
 print(seed)
 
 # генерация шума
 noise = PerlinNoise(octaves=octaves, seed=seed)
-# генерация матрицы
+# генерация матрицы и ее заполнение
 landscale = [[0 for i in range(terrain_width)] for i in range(terrain_width)]
-
-for position in range(terrain_width**2):
-   # вычисление высоты y в координатах (x, z)
-   x = floor(position / terrain_width)
-   z = floor(position % terrain_width)
-   y = floor(noise([x/period, z/period])*amp)
-   landscale[int(x)][int(z)] = int(y)
-
-image_file = "base.png"
-img = Image.open(image_file).convert('RGB')
-draw = ImageDraw.Draw(img)
-
-pixdata = img.load()
-height = 0
-# закрашиваем карту по значениям в матрице
-for position in range(terrain_width**2):
-   x = floor(position / terrain_width)
-   z = floor(position % terrain_width)
-   if landscale[int(x)][int(z)] < height:
-      draw.point((int(x), int(z)), (0,149,182)) #85, 72, 189
-   elif landscale[int(x)][int(z)] == height:
-      draw.point((int(x), int(z)), (252, 186, 3))
-   elif landscale[int(x)][int(z)] > height:
-      draw.point((int(x), int(z)), (19, 112, 21))
+fillMatrix(landscale, terrain_width)
+# заполнение изображения на основе значений в матрице
+fillPicture(landscale, terrain_width, height=0)
 
 img.save(f"outputs/output{seed}.png")
 
